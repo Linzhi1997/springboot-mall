d40d23a9ec2ab4c24f40c43dcec33c70eeb9690b
Serena <serenalin1997@gmail.com>
2024-08-22 17:53:06 +0800

    帳號功能 - 使用MD5加密密碼

--- src/main/java/com/serena/springbootmall/server/impl/UserServerImpl.java
+++ src/main/java/com/serena/springbootmall/server/impl/UserServerImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServerImpl implements UserServer {
@@ -23,6 +26,7 @@ public class UserServerImpl implements UserServer {
     public Integer register(UserRegisterRequest userRegisterRequest) {
         User user = userDao.getByEmail(userRegisterRequest.getEmail());
 
+        // 檢查註冊的email
         if(user!=null){
             // log 先寫
             log.warn("該 E-mail : {} 已經被註冊",userRegisterRequest.getEmail()); // 記錄警告信息
@@ -30,6 +34,11 @@ public class UserServerImpl implements UserServer {
             // 異常拋出後，會直接中斷方法的執行
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        // 使用MD5生成密碼雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
@@ -41,12 +50,16 @@ public class UserServerImpl implements UserServer {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getByEmail(userLoginRequest.getEmail());
-
+        // 檢查帳號
         if(user==null){
             log.warn("帳號 : {} ,尚未註冊",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        // 使用MD5生成密碼雜湊值 (登入)
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("帳號 : {}密碼不符,登入失敗",userLoginRequest.getEmail());
